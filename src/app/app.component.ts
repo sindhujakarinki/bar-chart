090e5cbe93f5f10f39d9e16864f0fd44aad07e45
sindhujakarinki <sindhujakarinki323@gmail.com>
2023-12-19 16:23:19 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexPlotOptions,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  @ViewChild('chart') chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'basic',
+          data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+        },
+      ],
+      chart: {
+        type: 'bar',
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        categories: [
+          'South Korea',
+          'Canada',
+          'United Kingdom',
+          'Netherlands',
+          'Italy',
+          'France',
+          'Japan',
+          'United States',
+          'China',
+          'Germany',
+        ],
+      },
+    };
+  }
+}
